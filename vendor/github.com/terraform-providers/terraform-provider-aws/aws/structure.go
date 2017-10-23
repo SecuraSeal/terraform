1c7cbff00ddcd9959c90e5097ed47aa1d13c15c8
Kevin Burke <kev@inburke.com>
2017-10-23 14:24:01 -0700

    Update terraform-aws provider

--- vendor/github.com/terraform-providers/terraform-provider-aws/aws/structure.go
+++ vendor/github.com/terraform-providers/terraform-provider-aws/aws/structure.go
@@ -233,6 +233,24 @@ func expandIPPerms(
 			}
 		}
 
+		if raw, ok := m["description"]; ok {
+			description := raw.(string)
+			if description != "" {
+				for _, v := range perm.IpRanges {
+					v.Description = aws.String(description)
+				}
+				for _, v := range perm.Ipv6Ranges {
+					v.Description = aws.String(description)
+				}
+				for _, v := range perm.PrefixListIds {
+					v.Description = aws.String(description)
+				}
+				for _, v := range perm.UserIdGroupPairs {
+					v.Description = aws.String(description)
+				}
+			}
+		}
+
 		perms[i] = &perm
 	}
 
@@ -466,9 +484,9 @@ func flattenHealthCheck(check *elb.HealthCheck) []map[string]interface{} {
 	return result
 }
 
-// Flattens an array of UserSecurityGroups into a []*ec2.GroupIdentifier
-func flattenSecurityGroups(list []*ec2.UserIdGroupPair, ownerId *string) []*ec2.GroupIdentifier {
-	result := make([]*ec2.GroupIdentifier, 0, len(list))
+// Flattens an array of UserSecurityGroups into a []*GroupIdentifier
+func flattenSecurityGroups(list []*ec2.UserIdGroupPair, ownerId *string) []*GroupIdentifier {
+	result := make([]*GroupIdentifier, 0, len(list))
 	for _, g := range list {
 		var userId *string
 		if g.UserId != nil && *g.UserId != "" && (ownerId == nil || *ownerId != *g.UserId) {
@@ -492,13 +510,15 @@ func flattenSecurityGroups(list []*ec2.UserIdGroupPair, ownerId *string) []*ec2.
 		}
 
 		if vpc {
-			result = append(result, &ec2.GroupIdentifier{
-				GroupId: id,
+			result = append(result, &GroupIdentifier{
+				GroupId:     id,
+				Description: g.Description,
 			})
 		} else {
-			result = append(result, &ec2.GroupIdentifier{
-				GroupId:   g.GroupId,
-				GroupName: id,
+			result = append(result, &GroupIdentifier{
+				GroupId:     g.GroupId,
+				GroupName:   id,
+				Description: g.Description,
 			})
 		}
 	}
@@ -2146,4 +2166,134 @@ func flattenFieldToMatch(fm *waf.FieldToMatch) []interface{} {
 		m["type"] = *fm.Type
 	}
 	return []interface{}{m}
+}
+
+// escapeJsonPointer escapes string per RFC 6901
+// so it can be used as path in JSON patch operations
+func escapeJsonPointer(path string) string {
+	path = strings.Replace(path, "~", "~0", -1)
+	path = strings.Replace(path, "/", "~1", -1)
+	return path
+}
+
+// Like ec2.GroupIdentifier but with additional rule description.
+type GroupIdentifier struct {
+	// The ID of the security group.
+	GroupId *string
+
+	// The name of the security group.
+	GroupName *string
+
+	Description *string
+}
+
+func expandCognitoIdentityPoolRoles(config map[string]interface{}) map[string]*string {
+	m := map[string]*string{}
+	for k, v := range config {
+		s := v.(string)
+		m[k] = &s
+	}
+	return m
+}
+
+func flattenCognitoIdentityPoolRoles(config map[string]*string) map[string]string {
+	m := map[string]string{}
+	for k, v := range config {
+		m[k] = *v
+	}
+	return m
+}
+
+func expandCognitoIdentityPoolRoleMappingsAttachment(rms []interface{}) map[string]*cognitoidentity.RoleMapping {
+	values := make(map[string]*cognitoidentity.RoleMapping, 0)
+
+	if len(rms) == 0 {
+		return values
+	}
+
+	for _, v := range rms {
+		rm := v.(map[string]interface{})
+		key := rm["identity_provider"].(string)
+
+		roleMapping := &cognitoidentity.RoleMapping{
+			Type: aws.String(rm["type"].(string)),
+		}
+
+		if sv, ok := rm["ambiguous_role_resolution"].(string); ok {
+			roleMapping.AmbiguousRoleResolution = aws.String(sv)
+		}
+
+		if mr, ok := rm["mapping_rule"].([]interface{}); ok && len(mr) > 0 {
+			rct := &cognitoidentity.RulesConfigurationType{}
+			mappingRules := make([]*cognitoidentity.MappingRule, 0)
+
+			for _, r := range mr {
+				rule := r.(map[string]interface{})
+				mr := &cognitoidentity.MappingRule{
+					Claim:     aws.String(rule["claim"].(string)),
+					MatchType: aws.String(rule["match_type"].(string)),
+					RoleARN:   aws.String(rule["role_arn"].(string)),
+					Value:     aws.String(rule["value"].(string)),
+				}
+
+				mappingRules = append(mappingRules, mr)
+			}
+
+			rct.Rules = mappingRules
+			roleMapping.RulesConfiguration = rct
+		}
+
+		values[key] = roleMapping
+	}
+
+	return values
+}
+
+func flattenCognitoIdentityPoolRoleMappingsAttachment(rms map[string]*cognitoidentity.RoleMapping) []map[string]interface{} {
+	roleMappings := make([]map[string]interface{}, 0)
+
+	if rms == nil {
+		return roleMappings
+	}
+
+	for k, v := range rms {
+		m := make(map[string]interface{})
+
+		if v == nil {
+			return nil
+		}
+
+		if v.Type != nil {
+			m["type"] = *v.Type
+		}
+
+		if v.AmbiguousRoleResolution != nil {
+			m["ambiguous_role_resolution"] = *v.AmbiguousRoleResolution
+		}
+
+		if v.RulesConfiguration != nil && v.RulesConfiguration.Rules != nil {
+			m["mapping_rule"] = flattenCognitoIdentityPoolRolesAttachmentMappingRules(v.RulesConfiguration.Rules)
+		}
+
+		m["identity_provider"] = k
+		roleMappings = append(roleMappings, m)
+	}
+
+	return roleMappings
+}
+
+func flattenCognitoIdentityPoolRolesAttachmentMappingRules(d []*cognitoidentity.MappingRule) []interface{} {
+	rules := make([]interface{}, 0)
+
+	for _, rule := range d {
+		r := make(map[string]interface{})
+		r["claim"] = *rule.Claim
+		r["match_type"] = *rule.MatchType
+		r["role_arn"] = *rule.RoleARN
+		r["value"] = *rule.Value
+
+		rules = append(rules, r)
+	}
+
+	return rules
 }
